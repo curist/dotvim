08769562cd9f62d26fe33593f8d93b50711a5947
curist <curist.cyc@gmail.com>
2021-05-25 11:24:55 +0800

    ugly ugly prefix/postifx filename check

--- config/plugins/fzf.lua
+++ config/plugins/fzf.lua
@@ -1,4 +1,5 @@
-local ignore = { 'COMMIT_EDITMSG' }
+local prefix_ignore = { '.git' }
+local suffix_ignore = { 'COMMIT_EDITMSG' }
 
 function starts_with(str, start)
    return str:sub(1, #start) == start
@@ -15,14 +16,26 @@ function _G._fzf_local_history()
   for _, v in ipairs(oldfiles) do
     if starts_with(v, pwd) then
       local ignored = false
-      for _, ig in ipairs(ignore) do
+      for _, ig in ipairs(suffix_ignore) do
         if ends_with(v, ig) then
           ignored = true
           break
         end
       end
       if not ignored then
-        table.insert(local_oldfiles, string.sub(v, #pwd + 2))
+      end
+      if not ignored then
+        local filepath = string.sub(v, #pwd + 2)
+        for _, ig in ipairs(prefix_ignore) do
+          if starts_with(filepath, ig) then
+            ignored = true
+            break
+          end
+        end
+
+        if not ignored then
+          table.insert(local_oldfiles, filepath)
+        end
       end
     end
   end
