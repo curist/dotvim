bcf8225205fd65837649d8cc48cfd26a1e92326a
curist <curist.cyc@gmail.com>
2021-05-26 13:32:45 +0800

    also filtered altfile based on MRU rules

--- config/plugins/fzf.lua
+++ config/plugins/fzf.lua
@@ -21,45 +21,63 @@ function get_all_files()
   return allfiles
 end
 
+function filter_filepath(pwd, orig_filepath)
+  local pwd = vim.call 'getcwd'
+
+  if not starts_with(orig_filepath, pwd) then
+    return false
+  end
+
+  local filepath = string.sub(orig_filepath, #pwd + 2)
+
+  for _, ig in ipairs(suffix_ignore) do
+    if ends_with(filepath, ig) then
+      return false
+    end
+  end
+
+  for _, ig in ipairs(prefix_ignore) do
+    if starts_with(filepath, ig) then
+      return false
+    end
+  end
+
+  return true
+end
+
+function filter_buffers(bufs)
+  local pwd = vim.call 'getcwd'
+  local filtered_bufs = {}
+  for _, v in ipairs(bufs) do
+    local filepath = vim.api.nvim_buf_get_name(v)
+    if filter_filepath(pwd, filepath) then
+      table.insert(filtered_bufs, v)
+    end
+    if #filtered_bufs > 1 then
+      break
+    end
+  end
+  return filtered_bufs
+end
+
 function _G._fzf_local_history()
   local allfiles = get_all_files()
   local pwd = vim.call 'getcwd'
   local local_oldfiles = {}
   for _, v in ipairs(allfiles) do
-    repeat
-      if not starts_with(v, pwd) then break end
-
+    if filter_filepath(pwd, v) then
       local filepath = string.sub(v, #pwd + 2)
-
-      local ignored = false
-      for _, ig in ipairs(suffix_ignore) do
-        if ends_with(filepath, ig) then
-          ignored = true
-          break
-        end
-      end
-
-      if ignored then break end
-
-      for _, ig in ipairs(prefix_ignore) do
-        if starts_with(filepath, ig) then
-          ignored = true
-          break
-        end
-      end
-
-      if ignored then break end
-
       table.insert(local_oldfiles, filepath)
-      break
-    until true
+    end
   end
   return local_oldfiles
 end
 
 function _G.altfile()
-  local bufs = vim.call('fzf#vim#_buflisted_sorted')
-  if #bufs > 1 then
+  local bufs = filter_buffers(vim.call('fzf#vim#_buflisted_sorted'))
+  if #bufs == 1 then
+    vim.api.nvim_set_current_buf(bufs[1])
+  elseif #bufs > 1 then
     vim.api.nvim_set_current_buf(bufs[2])
   end
 end
