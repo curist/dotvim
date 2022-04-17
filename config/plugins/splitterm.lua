15acc1d78eaffe98fcb5da4a659402430304b05f
curist <curist.cyc@gmail.com>
2022-04-17 15:21:05 +0800

    nvim create user command

--- config/plugins/splitterm.lua
+++ config/plugins/splitterm.lua
@@ -3,7 +3,7 @@ local scripts = require('dot.scripts')
 
 vim.keymap.set('n', '<m-t>', scripts.openTerm)
 vim.keymap.set('n', '<m-T>', dot.bind(scripts.openTerm, { use_cwd = true }))
-vim.api.nvim_add_user_command('VT', function(opts)
+vim.api.nvim_create_user_command('VT', function(opts)
   local cmd = opts.args
   local use_cwd = opts.bang
   scripts.openTerm({
