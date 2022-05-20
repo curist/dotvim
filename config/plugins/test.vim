b82dcfd8744089342a033551722456cd7eb85845
curist <curist.cyc@gmail.com>
2022-05-20 16:06:34 +0800

    zig test

--- config/plugins/test.vim
+++ config/plugins/test.vim
@@ -1,8 +1,9 @@
 let test#strategy = 'neovim'
-let test#neovim#term_position = "vert botright"
+let test#neovim#term_position = 'vert botright'
 
 nn <silent> <leader>tn :TestNearest<cr>
 nn <silent> <leader>tl :TestLast<cr>
 nn <silent> <leader>tf :TestFile<cr>
 
 let g:test#javascript#jest#executable = 'npx jest'
+
--- lua/dot/ts.lua
+++ lua/dot/ts.lua
@@ -183,4 +183,27 @@ function M.textobj.func()
   return get_node_textobj_range(node)
 end
 
+function M.run_nearest_zig_test()
+  local curr_node = ts_utils.get_node_at_cursor()
+  local function is_test_node(node)
+    return tostring(node) == '<node TestDecl>'
+  end
+  while curr_node and not is_test_node(curr_node) do
+    curr_node = curr_node:parent()
+  end
+  if not is_test_node(curr_node) then
+    return
+  end
+  local test_name = ts_utils.get_node_text(curr_node:named_child(0))[1]
+  local filename = vim.fn.expand('%:~:.')
+  local base_test_cmd = 'VT zig test --test-filter %s --cache-dir zig-cache %s'
+  vim.fn.execute((base_test_cmd):format(test_name, filename))
+end
+
+function M.run_zig_test()
+  local filename = vim.fn.expand('%:~:.')
+  local base_test_cmd = 'VT zig test --cache-dir zig-cache %s'
+  vim.fn.execute((base_test_cmd):format(filename))
+end
+
 return M
