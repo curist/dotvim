8374cbb50cfff53f1793f8765dc998eb8cd5dfc1
curist <curist.cyc@gmail.com>
2022-10-30 13:10:05 +0800

    ensure install tree-sitter-lx

--- config/plugins/treesitter.lua
+++ config/plugins/treesitter.lua
@@ -1,3 +1,12 @@
+local parser_config = require 'nvim-treesitter.parsers'.get_parser_configs()
+parser_config.lx = {
+  install_info = {
+    url = "~/.config/nvim/plugged/tree-sitter-lx",
+    files = {"src/parser.c"},
+    branch = "main",
+  },
+}
+
 require'nvim-treesitter.configs'.setup {
   ensure_installed = {
     "css",
@@ -13,17 +22,10 @@ require'nvim-treesitter.configs'.setup {
     "lua",
     "jsdoc",
     "json",
+    "lx",
   },
   highlight = {
     enable = true,
   },
 }
 
-local parser_config = require 'nvim-treesitter.parsers'.get_parser_configs()
-parser_config.lx = {
-  install_info = {
-    url = "~/.config/nvim/plugged/tree-sitter-lx",
-    files = {"src/parser.c"},
-    branch = "main",
-  },
-}
