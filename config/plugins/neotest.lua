45647a3aec10dbc6cb6ebae5d0584575d4112d20
curist <curist.cyc@gmail.com>
2024-11-13 12:55:53 +0800

    neotest keymaps
    to show test output

--- config/plugins/neotest.lua
+++ config/plugins/neotest.lua
@@ -4,6 +4,8 @@ local keymaps = {
   { "<leader>tr", function() require("neotest").run.run() end, desc = "Run Nearest" },
   { "<leader>tl", function() require("neotest").run.run_last() end, desc = "Run Last" },
   { "<leader>ts", function() require("neotest").summary.toggle() end, desc = "Toggle Summary" },
+  { "<leader>to", function() require("neotest").output.open({ enter = true, last_run = true }) end, desc = "Show last test output" },
+  { "<leader>tO", function() require("neotest").output_panel.toggle() end, desc = "Toggle output panel" },
 }
 
 for _, keymap in ipairs(keymaps) do
