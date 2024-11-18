ef97b847bbd592e03a26debe58f74fa1932546f5
curist <curist.cyc@gmail.com>
2024-11-18 10:34:48 +0800

    using treesitter textobjects
    for vaf alone, i guess

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,46 +1,67 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  depedencies = {
-    "moonbit-community/moonbit.nvim",
-    "curist/tree-sitter-lx",
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    event = "BufRead",
   },
-  build = ":TSUpdate",
-  event = "BufRead",
-  opts = {
-    ensure_installed = {
-      "c",
-      "css",
-      "go",
-      "javascript",
-      "typescript",
-      "html",
-      "lua",
-      "jsdoc",
-      "json",
+  {
+    'nvim-treesitter/nvim-treesitter',
+    depedencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+      'moonbit-community/moonbit.nvim',
+      'curist/tree-sitter-lx',
     },
-    highlight = { enable = true },
-    indent = { enable = true },
-    matchup = {
-      enable = true,
-      disable_virtual_text = true,
-    },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "+",
-        node_incremental = "+",
-        node_decremental = "-",
-        scope_incremental = false,
+    build = ":TSUpdate",
+    event = "BufRead",
+    opts = {
+      ensure_installed = {
+        "c",
+        "css",
+        "go",
+        "javascript",
+        "typescript",
+        "html",
+        "lua",
+        "jsdoc",
+        "json",
+      },
+      highlight = { enable = true },
+      indent = { enable = true },
+      matchup = {
+        enable = true,
+        disable_virtual_text = true,
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "+",
+          node_incremental = "+",
+          node_decremental = "-",
+          scope_incremental = false,
+        },
+      },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = false,
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+          },
+          selection_modes = {
+            ['@function.outer'] = 'V',
+          },
+        },
       },
     },
-  },
-  config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
-  end,
-  keys = {
-    { 'gs', function() require 'dot.ts'.print_node_at_cursor() end },
-    { 'gt', function() require'dot.ts'.goto_top_node_at_cursor() end },
-    { '<c-j>', function() require'dot.ts'.goto_next_top_node() end },
-    { '<c-k>', function() require'dot.ts'.goto_prev_top_node() end },
-  },
-}
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+    keys = {
+      { 'gs', function() require 'dot.ts'.print_node_at_cursor() end },
+      { 'gt', function() require'dot.ts'.goto_top_node_at_cursor() end },
+      { '<c-j>', function() require'dot.ts'.goto_next_top_node() end },
+      { '<c-k>', function() require'dot.ts'.goto_prev_top_node() end },
+    },
+  }
+    }
+
