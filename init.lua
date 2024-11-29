d7b3c42f6562ceff954efec91288d54e23336dbf
curist <curist.cyc@gmail.com>
2024-11-29 11:21:40 +0800

    lua/plugins -> specs; /plugins -> /plugin
    neovim autoloads /plugin/**/*.{lua,vim}

--- init.lua
+++ init.lua
@@ -1,24 +1,23 @@
 -- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 
 vim.cmd('runtime! config/*.{vim,lua}')
-vim.cmd('runtime! plugins/*.{vim,lua}')
 
 -- Setup lazy.nvim
-require("lazy").setup({
+require('lazy').setup({
   spec = {
-    { import = "plugins" },
+    { import = 'specs' },
   },
   defaults = {
     lazy = false,
     version = false, -- always use the latest git commit
   },
-  install = { colorscheme = { "catppuccin", "sorbet" } },
+  install = { colorscheme = { 'catppuccin', 'sorbet' } },
   checker = {
     enabled = true,
     notify = false,
@@ -28,14 +27,14 @@ require("lazy").setup({
     rtp = {
       -- disable some rtp plugins
       disabled_plugins = {
-        "gzip",
-        "matchit",
-        "matchparen",
-        "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
+        'gzip',
+        'matchit',
+        'matchparen',
+        'netrwPlugin',
+        'tarPlugin',
+        'tohtml',
+        'tutor',
+        'zipPlugin',
       },
     },
   },
--- lua/specs/mason.lua
+++ lua/specs/mason.lua
@@ -1,24 +1,25 @@
 return {
   {
     'williamboman/mason-lspconfig.nvim',
+    event = { 'BufReadPre', 'BufNewFile' },
     dependencies = {
       'williamboman/mason.nvim',
       'neovim/nvim-lspconfig',
     },
     config = function()
-      require('mason-lspconfig').setup {
+      require('mason-lspconfig').setup({
         ensure_installed = {
           'lua_ls',
           'denols',
           'gopls',
         },
         automatic_installation = true,
-      }
-      require('mason-lspconfig').setup_handlers {
+      })
+      require('mason-lspconfig').setup_handlers({
         function(server_name)
-          require('lspconfig')[server_name].setup {}
+          require('lspconfig')[server_name].setup({})
         end,
-      }
+      })
     end,
   },
   {
