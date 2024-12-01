6548964dde6c8899e31f2d52bc638b308f0f1a1a
curist <curist.cyc@gmail.com>
2024-12-01 21:10:05 +0800

    cursor scope diagnostic

--- lua/specs/lsp.lua
+++ lua/specs/lsp.lua
@@ -1,5 +1,24 @@
-vim.diagnostic.config({ virtual_text = false })
-vim.keymap.set('n', '<leader>ld', vim.diagnostic.open_float, { desc = 'Diagnostic' })
+vim.diagnostic.config({
+  -- underline = false,
+  virtual_text = false,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = '󰅚 ',
+      [vim.diagnostic.severity.WARN] = '󰀪 ',
+      [vim.diagnostic.severity.HINT] = '󰌶 ',
+      [vim.diagnostic.severity.INFO] = ' ',
+    },
+  },
+  float = {
+    close_events = { 'BufLeave', 'CursorMoved', 'InsertEnter' },
+    border = 'rounded',
+    source = 'if_many',
+    prefix = ' ',
+    scope = 'cursor',
+  },
+})
+
+-- vim.keymap.set('n', '<leader>ld', vim.diagnostic.open_float, { desc = 'Diagnostic' })
 vim.keymap.set('n', '<leader>lq', vim.diagnostic.setqflist, { desc = 'Send diagnostic to quickfix' })
 
 vim.api.nvim_create_autocmd('LspAttach', {
@@ -32,9 +51,16 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local client = vim.lsp.get_client_by_id(event.data.client_id)
     if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
       nn('<leader>lh', function()
-        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
       end, 'Toggle inlay hints')
     end
+
+    vim.api.nvim_create_autocmd('CursorHold', {
+      buffer = event.buf,
+      callback = function()
+        vim.diagnostic.open_float()
+      end,
+    })
   end,
 })
 
