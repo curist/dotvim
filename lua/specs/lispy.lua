e4f5a9fb206d180273b5b5271adbe825906dabb1
curist <curist.cyc@gmail.com>
2024-12-10 13:03:26 +0800

    lispy, again

---
+++ lua/specs/lispy.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    'jaawerth/fennel.vim',
+    ft = 'fennel',
+  },
+  {
+    'udayvir-singh/tangerine.nvim',
+    ft = 'fennel',
+    opts = {},
+  },
+  {
+    'guns/vim-sexp',
+    ft = { 'fennel' },
+    init = function()
+      vim.g.sexp_enable_insert_mode_mappings = false
+      vim.g.sexp_filetypes = 'clojure,scheme,lisp,timl,fennel'
+      vim.g.sexp_mappings = {
+        sexp_swap_list_backward = '',
+        sexp_swap_list_forward = '',
+        sexp_swap_element_backward = '',
+        sexp_swap_element_forward = '',
+        sexp_emit_head_element = '',
+        sexp_emit_tail_element = '',
+        sexp_capture_prev_element = '',
+        sexp_capture_next_element = '',
+      }
+    end,
+  },
+  {
+    'tpope/vim-sexp-mappings-for-regular-people',
+    dependencies = { 'guns/vim-sexp' },
+    ft = { 'fennel' },
+  },
+}
