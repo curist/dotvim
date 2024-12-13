dd5085d028b4123adbbfd08dbc6bf2d8795c27b7
curist <curist.cyc@gmail.com>
2024-12-13 13:40:57 +0800

    add luarocks to rtp
    so conjure can find fennel as our dep

--- init.lua
+++ init.lua
@@ -8,6 +8,10 @@ vim.opt.rtp:prepend(lazypath)
 
 vim.cmd('runtime! config/*.{vim,lua}')
 
+-- Add luarocks to rtp
+local home = vim.uv.os_homedir()
+package.path = package.path .. ';' .. home .. '/.luarocks/share/lua/5.1/?/init.lua;'
+package.path = package.path .. ';' .. home .. '/.luarocks/share/lua/5.1/?.lua;'
 -- Setup lazy.nvim
 require('lazy').setup({
   spec = {
