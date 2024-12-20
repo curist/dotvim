e941c0eef23994ca73e25659281c5bb2400a71bb
curist <curist.cyc@gmail.com>
2024-12-20 21:28:51 +0800

    cmp mappings

--- lua/specs/cmp.lua
+++ lua/specs/cmp.lua
@@ -20,6 +20,7 @@ return {
         ['<c-u>'] = cmp.mapping.scroll_docs(-4),
         ['<c-d>'] = cmp.mapping.scroll_docs(4),
         ['<c-j>'] = cmp.mapping.confirm({ select = true }),
+        ['<c-;>'] = cmp.mapping.complete(),
         ['<c-e>'] = cmp.mapping({
           i = function(fallback)
             fallback()
@@ -27,14 +28,6 @@ return {
         }),
         ['<cr>'] = cmp.mapping({
           i = function(fallback)
-            if cmp.visible() and cmp.get_active_entry() then
-              cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-            else
-              fallback()
-            end
-          end,
-          s = cmp.mapping.confirm({ select = true }),
-          c = function(fallback)
             if cmp.visible() and cmp.get_active_entry() then
               cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
             else
