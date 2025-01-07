82c32ec6f6d1ea547304a7c5ebe432448e36854e
curist <curist.cyc@gmail.com>
2025-01-07 10:59:54 +0800

    enable gzip/tar/zip plugins

--- init.lua
+++ init.lua
@@ -33,14 +33,11 @@ require('lazy').setup({
     rtp = {
       -- disable some rtp plugins
       disabled_plugins = {
-        'gzip',
         'matchit',
         'matchparen',
         'netrwPlugin',
-        'tarPlugin',
         'tohtml',
         'tutor',
-        'zipPlugin',
       },
     },
   },
