1980a9923055b84f13ddb8fdf1412d19d4c47abe
curist <curist.cyc@gmail.com>
2025-01-13 12:38:13 +0800

    overseer & bb-end template

---
+++ lua/overseer/template/bb-edn.lua
@@ -0,0 +1,86 @@
+local bbscript = [[
+(require '[clojure.edn :as edn])
+(require '[cheshire.core :as json])
+
+(assert *command-line-args*)
+(def bbedn (slurp (first *command-line-args*)))
+(def template (edn/read-string bbedn))
+(def tasks (->> (keys (:tasks template))
+                (filter symbol?)
+                (map #(let [n (name %)
+                            t (get-in template [:tasks %])]
+                        {:name n
+                         :doc (:doc t)
+                         :args (not= nil
+                                     (re-find #"\*command-line-args\*"
+                                              (pr-str (:task t))))}))))
+
+(print (json/encode tasks))
+]]
+
+local bb_script_path = vim.fn.tempname()
+vim.fn.writefile({ bbscript:gsub('\n', '') }, bb_script_path)
+vim.print(bb_script_path)
+
+local function get_bb_edn(opts)
+  return vim.fs.find('bb.edn', { upward = true, type = 'file', path = opts.dir })[1]
+end
+
+return {
+  cache_key = get_bb_edn,
+  condition = {
+    callback = function(opts)
+      if vim.fn.executable('bb') == 0 then
+        return false, 'Command "bb" not found'
+      end
+      if not get_bb_edn(opts) then
+        return false, 'No "bb.edn" file found'
+      end
+      return true
+    end,
+  },
+  generator = function(opts, cb)
+    local json = require('overseer.json')
+    local log = require('overseer.log')
+
+    local ret = {}
+
+    local bbedn = get_bb_edn(opts)
+
+    local jid = vim.fn.jobstart({ 'bb', bb_script_path, bbedn }, {
+      stdout_buffered = true,
+      on_stdout = vim.schedule_wrap(function(_, output)
+        local result = json.decode(output[1])
+        for _, task in ipairs(result) do
+          table.insert(ret, {
+            priority = 60,
+            name = task.name,
+            desc = task.doc,
+            params = {
+              args = {
+                optional = not task.args,
+                type = 'list',
+                delimiter = ' ',
+              },
+            },
+            builder = function(params)
+              return {
+                cmd = { 'bb', task.name },
+                args = params.args,
+                cwd = params.cwd,
+              }
+            end,
+          })
+        end
+      end),
+      on_exit = vim.schedule_wrap(function()
+        cb(ret)
+      end),
+    })
+
+    if jid <= 0 then
+      log:error('Failed to execute bb')
+      cb(ret)
+    end
+  end,
+}
